7167fefd323de6121d2f85d983d0b3772b8c4694
Zetrance <zetrancexr@gmail.com>
2025-03-29 12:15:26 +0530

    Update PhotoPage.tsx-sandeep6

--- src/pages/PhotoPage.tsx
+++ src/pages/PhotoPage.tsx
@@ -141,7 +141,7 @@ const PhotoPage = () => {
                     maxWidth: '100%',
                     maxHeight: '70vh',
                     objectFit: 'cover',
-                    transform: 'scaleX(1) rotate(0deg)',
+                    transform: 'scaleX(1) rotate(180deg)',
                     transformOrigin: 'center',
                   }}
                 />
